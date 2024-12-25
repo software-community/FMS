edab9824e6cd3f72f19a04a7e51150f3cbf3136e
Nalin Angrish <nalin@nalinangrish.me>
2024-12-25 18:34:53 +0530

    Link MongoDB Database.

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 var cors = require('cors')
 
@@ -12,6 +13,12 @@ const mainRouter = require('./routes/index');
 app.use('/', mainRouter);
 
 const port = process.env.BACKEND_PORT || 5000;
-app.listen(port, () => {
-  console.log(`Backend service listening at http://localhost:${port}`);
+mongoose.connect('mongodb+srv://user:superstrongpassword@testing.f3qii.mongodb.net/?retryWrites=true&w=majority&appName=Testing').then(()=>{
+  console.log("DB Connected")
+  app.listen(port, () => {
+    console.log(`Backend service listening at http://localhost:${port}`);
+  });
+}).catch((err)=>{
+  console.log("DB Connection Error");
+  console.error(err)
 });
