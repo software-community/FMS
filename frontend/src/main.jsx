53b46c59eafff932332f699655d9f893df72dd98
Nalin Angrish <nalin@nalinangrish.me>
2024-12-28 18:39:22 +0530

    Ye sab add kiya hai

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 // Import pages here
 import Home from './pages/home/Home';
+import Signin from './pages/signin/Signin';
 // import Login from './pages/login/Login';
 // import SignUp from './pages/signup/Signup';
 // and so on....
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
       {/* <Route path="/" element={<Login />} /> */}
       {/* <Route path="/" element={<SignUp />} /> */}
       {/* And so on.... */}
