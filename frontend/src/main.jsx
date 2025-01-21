bddd434632d39ed8f455d91ce7b02d9084e5a74e
Hansika <hansika7012@gmail.com>
2025-01-21 23:44:51 +0530

    tried to troubleshoot2.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/" element={<Login />} /> */}
+      { <Route path="/" element={<Dashboard />} /> }
       {/* <Route path="/" element={<SignUp />} /> */}
       {/* And so on.... */}
     </Routes>
