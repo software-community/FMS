972fe47fc7d055aabd15c6181e70762639de8c2b
Hansika <hansika7012@gmail.com>
2025-01-21 23:58:07 +0530

    troubleshoot5.0

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -4,32 +4,33 @@ import viteLogo from '/vite.svg'
 import './dashboard.css'
 
 function Dashboard() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <video id="home-video" autoPlay muted loop>
-        <source src="https://www.festivalpro.com/videos/homepage.mp4" type="video/mp4" />
-      </video>
-      <nav className="navbar">
-        <div className="iitrpr">
-          IIT ROPAR
+          <source src="https://www.festivalpro.com/videos/homepage.mp4" type="video/mp4" />
+        </video>
+        <nav className="navbar">
+          <div className="iitrpr">
+            IIT ROPAR
+          </div>
+          <div className="navlist">
+            <ul>
+              <li><a href="#home">HOME</a></li>
+              <li><a href="#events">EVENTS</a></li>
+              <li><a href="#about">ABOUT</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+            </ul>
+          </div>
+        </nav>
+  
+        <div className="container">
+          <h1>Fests</h1>
+          <p>ORGANIZE AND RELAX</p>
         </div>
-        <div className="navlist">
-          <ul>
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#events">EVENTS</a></li>
-            <li><a href="#about">ABOUT</a></li>
-            <li><a href="#contact">CONTACT</a></li>
-          </ul>
-        </div>
-      </nav>
-
-      <div className="container">
-        <h1>Fests</h1>
-        <p>ORGANIZE AND RELAX</p>
-      </div>
-
     </>
-  );
+  )
 }
 
 export default Dashboard;
