1da9418063c94da7de85d9cbe05481eae3f134bc
Hansika <hansika7012@gmail.com>
2025-01-23 16:34:32 +0530

    alignment issue fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       {/*<Route path="/" element={<Home />} />*/}
-      { <Route path="/" element={<Dashboard />} /> }
+      { <Route path="/dashboard" element={<Dashboard />} /> }
       {/* <Route path="/" element={<SignUp />} /> */}
       {/* And so on.... */}
     </Routes>
